c2f749b77d61a2978b764a06b3924cb37a1918eb
SwatiArjun <swatiadakalgundi@gmail.com>
2025-07-24 12:08:01 +0530

    backend done

---
+++ backend/controller/usercontroller.js
@@ -0,0 +1,96 @@
+const e = require('express');
+const usermodel=require('../model/usermodel')
+const bcrypt=require('bcryptjs')
+const registeruser=async(req,res)=>{
+    try{
+         let {username,email,password}=req.body;
+         if(!username || !email || !password){
+            return res.status(400).json({error: 'username ,email,password, are required'})
+         }
+         let user=await usermodel.findOne({email});
+         if(user){
+            return res.status(400).json({error:'user already exists'})
+         }
+         req.body.password=await bcrypt.hash(password,10);
+         let newuser=new usermodel(req.body);
+         await newuser.save();
+         res.status(200).json({message:"user registerd successfully",user:newuser})
+    }catch(error){
+         res.status(500).json({error:'internal server error'})
+    }
+}
+const login=async (req,res)=>{
+   try{
+       const{email,password}=req.body;
+       if(!email || !password){
+            return res.status(400).json({error:'email,password are requierd'})
+       }
+       let user =await usermodel.findOne({email});
+       if(!user){
+         return res.status(400).json({error:'user not found'});
+       }
+       const ismatch=await bcrypt.compare(password,user.password);
+       if(!ismatch){
+         return res.status(400).json({error:'password not match'});
+       }
+       res.status(200).json({message:"user logged successfully", user:user})
+   }catch(error){
+      res.status(500).json({error:'internal server error'})
+   }
+}
+const getusers=async(req,res)=>{
+   try{
+      const users=await usermodel.find().select('-password');
+      return res.status(200).json({message:"users fetched successfully",users:users})
+   }catch(error){
+      return res.status(500).json({error:'internal server error'})
+   }
+}
+const getuserbyemail=async(req,res)=>{
+   try{
+      const email=req.params.email;
+      if(!email){
+         return res.status(400).json({error:'email is required'})
+      }
+      const user=await usermodel.findOne({email}).select("-password")
+      if(!user){
+         return res.status(400).json({error:"user not found"})
+      }
+      return res.status(200).json({message:"user fetched",user:user})
+   }catch(error){
+      return res.status(500).json({error:'internal server error',error:error})
+   }
+}
+const updateuser=async(req,res)=>{
+     try{
+      const id=req.params.id;
+      if(!id){
+         return  res.status(400).json({error:'id is required'})
+      }
+      const updateuser=await usermodel.findByIdAndUpdate(id,req.body,{new:true});
+      if(!updateuser){
+         return res.status(404).json({error:"update failed user not found"})
+      }
+      return res.status(200).json({message:"user updated successfully",user:updateuser})
+     }catch(error){
+      console.log(error);
+      return  res.status(500).json({error:'internal server error'})
+     }
+}
+
+const deleteuser=async(req,res)=>{
+   try{
+     const id=req.params.id;
+     if(!id){
+      return res.status(400).json({error:'id is required'})
+     }
+     const deleteduser=await usermodel.findByIdAndDelete(id);
+     if(!deleteduser){
+      return res.status(404).json({error:"delete failed user not found"})
+     }
+     return res.status(200).json({message:"user deleted successfully",user:deleteduser})
+   }catch(error){
+     return res.status(500).json({error:"initernal server error"})
+   }
+}
+module.exports={registeruser,login,getusers,getuserbyemail,updateuser,deleteuser}
---
+++ backend/db/coonectdb.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+
+async function connect(){
+    try{
+      await mongoose.connect('mongodb://localhost:27017/ECOMMERCE')
+      console.log('db connected');
+    }catch(error){
+       console.log('error while connecting to db');
+    }
+}
+
+module.exports=connect
